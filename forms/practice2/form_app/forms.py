faefa3be58936e86be5f8237aff643d037df08f3
rahulbhaskar <bhaskar.rahul25@gmail.com>
2018-01-05 16:52:36 +0530

    validation added

--- forms/practice2/form_app/forms.py
+++ forms/practice2/form_app/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 class FormName(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    text = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(help_text='Name')
+    email = forms.EmailField(help_text='Email Address')
+    text = forms.CharField(help_text='Message',widget=forms.Textarea)
+
+    def clean(self): #evoked only when submit button is pressed
+        cleaned_data = super().clean() #returns a dict {'name': 'amit', 'email': 'amit@gmail.com', 'text': 'hello'}
+        name = cleaned_data['name']
+        if name[0].lower() == 'a':
+            raise forms.ValidationError("Dont start with a")

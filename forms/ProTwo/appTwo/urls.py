5fa8d1a7daa01404bcc58106e4684b2f6f7ad06a
rahulbhaskar <bhaskar.rahul25@gmail.com>
2018-01-05 19:16:55 +0530

    modelfrom done

--- forms/ProTwo/appTwo/urls.py
+++ forms/ProTwo/appTwo/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url
 from appTwo import views
-from appTwo import forms
 
 urlpatterns = [
     url(r'^$',views.users,name='users'),
-    url(r'^form/', forms.ModelFormUser, name='form')
-
 ]
--- modelforms/ProTwo/appTwo/views.py
+++ modelforms/ProTwo/appTwo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from appTwo.models import User
+from appTwo.forms import ModelFormUser
 # Create your views here.
 
 def index(request):
@@ -11,3 +12,16 @@ def users(request):
     user_list = User.objects.order_by('first_name')
     user_dict = {"users":user_list}
     return render(request,'apptwo/users.html',context=user_dict)
+
+def form_page(request):
+    form = ModelFormUser()
+
+    if request.method == "POST":
+        form = ModelFormUser(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('Error Form Invalid')
+
+    return render(request,'appTwo/form.html',{'form':form})

51716d9f04d7bb5a345cce97bbfb78daacf6c7d7
rahulbhaskar <bhaskar.rahul25@gmail.com>
2018-01-06 23:34:24 +0530

    Done

--- pycharm/Database/app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from app.models import UserModel
-
-class UserForm(forms.ModelForm):
-    class Meta():
-        model = UserModel
-        fields = '__all__'
--- pycharm/Database/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from app.views import formview, database
-
-app_name = 'app'
-
-urlpatterns = [
-    url(r'^database/', database, name='database'),
-    url(r'^form/', formview, name='form'),
-]

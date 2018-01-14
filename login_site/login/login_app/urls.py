0c6c83b1c405fb405249d40711302059284c81d2
rahulbhaskar <bhaskar.rahul25@gmail.com>
2018-01-14 17:43:07 +0530

    lecture 5 deleteed

--- login_site/login/login_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from login_app import views
-
-app_name = 'login_app'
-
-urlpatterns = [
-    path(r'^register/', views.register ,name='register'),
-]
--- login_site/website/webapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from webapp.forms import UserForm
-
-# Create your views here.
-def index(request):
-    return  render(request, 'webapp/index.html')
-
-def user_form_page(request):
-    form =

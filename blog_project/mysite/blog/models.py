3e3d5dd983b1ee147975711f8eca6d78e13a6abc
Rahul Bhaskar <bhaskar.rahul25@gmail.com>
2018-02-13 19:15:06 +0530

    post_detail remains

--- blog_project/mysite/blog/models.py
+++ blog_project/mysite/blog/models.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
-    title = models.CharField(max_lenght=256)
+    title = models.CharField(max_length=256)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True,null=True)
@@ -18,7 +18,7 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk':self.pk},)
+        return reverse("blog:post_detail", kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -53,15 +53,14 @@ class DraftListView(LoginRequiredMixin, generic.ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__isnull=True)/
-        .order_by('created_date')
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
 
 @login_required
 def post_publish(request, pk):
-    post = get_object_or_404(request, pk=pk)
-    post.publish
-    return redirect('post_detail', pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog:post_detail', pk=pk)
 
 ################################################################
 ################################################################
@@ -78,7 +77,7 @@ def add_comment_to_post(request,pk):
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('blog:post_detail', pk=post.pk)
     else:
         form = CommentForm()
     return render(request, 'blog/comment_form.html',{'form':form})
@@ -87,7 +86,7 @@ def add_comment_to_post(request,pk):
 def comment_approve(request,pk):
     comment = get_object_or_404(Comment,pk=pk)
     comment.approve()
-    return redirect('post_detail',pk=comment.post.pk)
+    return redirect('blog:post_detail',pk=comment.post.pk)
 
 
 
@@ -97,4 +96,4 @@ def comment_remove(request,pk):
     comment = get_object_or_404(Comment,pk=pk)
     post_pk = comment.post.pk
     comment.delete()
-    return redirect('post_detail',pk=post_pk)
+    return redirect('blog:post_detail',pk=post_pk)

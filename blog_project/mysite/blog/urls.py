a4f887354ee04e4aa923f4f7a81b6456eda398cc
Rahul Bhaskar <bhaskar.rahul25@gmail.com>
2018-02-13 13:03:29 +0530

    view and url complete

--- blog_project/mysite/blog/urls.py
+++ blog_project/mysite/blog/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
         name='about'),
 
     url(
-        r'^post/(?P<pk>\d+$)', 
+        r'^post/(?P<pk>\d+)$', 
         views.PostDetailView.as_view(), 
         name='post_detail'),
 
@@ -25,12 +25,12 @@ urlpatterns = [
         name='post_new'),
 
     url(
-        r'^post/(?P<pk>\d+$)/edit/$', 
+        r'^post/(?P<pk>\d+)/edit/$', 
         views.PostUpdateView.as_view(), 
         name='post_edit'),
 
     url(
-        r'^post/(?P<pk>\d+$)/remove/$', 
+        r'^post/(?P<pk>\d+)/remove/$', 
         views.PostDeleteView.as_view(), 
         name='post_remove'),
     
@@ -39,4 +39,25 @@ urlpatterns = [
         views.DraftListView.as_view(), 
         name='post_draft_list'),
 
+    url(
+        r'^post/(?P<pk>\d+)/publish/$',
+        views.post_publish,
+        name='post_publish'),
+
+    url(
+        r'^post/(?P<pk>\d+)/comment/$',
+        views.add_comment_to_post, 
+        name='add_comment_to_post'),
+
+    url(
+        r'^comment/(?P<pk>\d+)/approve/$',
+        views.comment_approve,
+        name='comment_approve'),
+
+    url(
+        r'^comment/(?P<pk>\d+)/remove/$',
+        views.comment_remove,
+        name='comment_remove),
+
+    
 ]
--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
-from .models import Post
+from .models import Post, Comment
 from blog.forms import PostForm, CommentForm
 from django.utils import timezone
 from django.urls import reverse_lazy
@@ -56,6 +56,13 @@ class DraftListView(LoginRequiredMixin, generic.ListView):
         return Post.objects.filter(published_date__isnull=True)/
         .order_by('created_date')
 
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(request, pk=pk)
+    post.publish
+    return redirect('post_detail', pk=pk)
+
 ################################################################
 ################################################################
 ################################################################
@@ -75,3 +82,19 @@ def add_comment_to_post(request,pk):
     else:
         form = CommentForm()
     return render(request, 'blog/comment_form.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+
+
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
